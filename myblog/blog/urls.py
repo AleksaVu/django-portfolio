0546bb3fa9d0480bf529c088d176e38f3e15cc6e
AleksaVu <vujosevic.aleksa404@gmail.com>
2022-01-28 16:47:29 +0100

    Add blog post search option, and post_searched.html template. Update blog_post.html

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_listing, name='post-listing'),    
+    path('blog-post/<int:post_id>/', views.blog_post, name='blog-post'),  
+    path('blog-post/search/', views.post_search, name='post-search'),  
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from .models import Post
 
 def post_listing(request):
-    posts = Post.objects.all()
-    
+    posts = Post.objects.all()    
     return render(request, 'blog/post_listing.html', {'posts': posts})
 
+def blog_post(request, post_id):
+    post = Post.objects.get(id=post_id)    
+    return render(request, 'blog/blog_post.html', {'post': post})
+
+def post_search(request):
+    """ A view of searched post"""
+    queried_post = request.GET.get('q')    
+    post = None
+    try:
+        post = Post.objects.get(title__iexact=queried_post)        
+        queried_post_exists = True
+    except:
+        queried_post_exists = False
+    return render(request, 'blog/post_searched.html', {'post': post , 'results': queried_post_exists})
