2dbe27713ba2e4d2e03754a61cb499c34458fb6b
AleksaVu <vujosevic.aleksa404@gmail.com>
2022-01-28 19:37:54 +0100

    Add "last blog posts" section to homepage.html template

--- myblog/cms/views.py
+++ myblog/cms/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from projects.models import Project
+from blog.models import Post
 
 
 def homepage(request):
-    projects = Project.objects.all().order_by('id')[:3]
-    return render(request, 'cms/homepage.html', {'projects': projects})
+    projects = Project.objects.all().order_by('id')[:4]
+    posts = Post.objects.all().order_by('id')[:3]
+    return render(request, 'cms/homepage.html', {'projects': projects, 'posts':posts})
 
 
 def aboutme(request):
